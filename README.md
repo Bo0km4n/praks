fd20dcd24584929ae876a5dc89c6cc801be93f2e
Bo0km4n <pokopenkatuya@gmail.com>
2018-06-15 13:47:35 +0900

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# Praks
+
+praks is a library for dynamically generating golang structure code from json and csv text data.
+
+Supported text style:
+    - json
+    - csv <= will
+
+## Installation
+
+Use the `go` command:
+
+```
+$ go get github.com/Bo0km4n/praks
+```
+
+## Example
+
+```go
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/Bo0km4n/praks"
+)
+
+func main() {
+    jsonBody := `{"charset":"utf-8","client_id":"dfd0ba49-d8ad-4c53-86e5-b05018ae5b90", "nested": {"col0": 1, "col1": "hello"}}`
+
+	p := praks.NewParser("json")
+	s := p.TexToStruct(jsonBody)
+	fmt.Println(s.GetValue("CHARSET"), s.GetValue("CLIENT_ID"), s.GetValue("NESTED"))
+}
+```
---
+++ struct_test.go
@@ -0,0 +1,97 @@
+package praks
+
+import (
+	"bufio"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetFieldNames(t *testing.T) {
+
+	tc := []struct {
+		Parser   string
+		FilePath string
+		Expect   int
+	}{
+		{
+			Parser:   "json",
+			FilePath: "example/sample.json",
+			Expect:   2,
+		},
+	}
+
+	for _, c := range tc {
+		p := NewParser(c.Parser)
+		fp, err := os.Open(c.FilePath)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(fp)
+		scanner.Scan()
+		s := p.TexToStruct(scanner.Text())
+		f := s.GetFieldNames()
+		assert.Equal(t, c.Expect, len(f))
+	}
+}
+
+func TestGetValueTypeToStringJSON(t *testing.T) {
+	tc := []struct {
+		Parser   string
+		FilePath string
+		Expect   string
+	}{
+		{
+			Parser:   "json",
+			FilePath: "example/sample.json",
+			Expect:   "string",
+		},
+	}
+
+	for _, c := range tc {
+		p := NewParser(c.Parser)
+		fp, err := os.Open(c.FilePath)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(fp)
+		scanner.Scan()
+		s := p.TexToStruct(scanner.Text())
+		assert.Equal(t, c.Expect, s.GetValueTypeToString("CHARSET"))
+	}
+}
+
+func TestGetFieldAndType(t *testing.T) {
+	tc := []struct {
+		Parser   string
+		FilePath string
+		Keys     []string
+	}{
+		{
+			Parser:   "json",
+			FilePath: "example/sample.json",
+			Keys:     []string{"CHARSET", "CLIENT_ID"},
+		},
+	}
+
+	for _, c := range tc {
+		p := NewParser(c.Parser)
+		fp, err := os.Open(c.FilePath)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(fp)
+		scanner.Scan()
+		s := p.TexToStruct(scanner.Text())
+		d := s.GetFieldAndType()
+
+		for _, k := range c.Keys {
+			_, ok := d[k]
+			assert.True(t, ok)
+		}
+	}
+}
